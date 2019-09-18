b312d342d2c72637627b9f6f8ed9f92cfe87696b
Alexandr Pogodin <37239276+AlexandrPogodin@users.noreply.github.com>
2019-09-18 20:55:18 +0400

    Update [*]

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
 
 server.listen(3000);
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html');
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
 });
 
 users = [];
 connections = [];
 
-io.sockets.on('connection', (socket) => {
-    console.log('Успешное соединение');
-    connections.push(socket);
+io.sockets.on("connection", socket => {
+  console.log("Connect");
+  connections.push(socket);
 
-    socket.on('disconnect', (data) => {
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('Отключились');
-    });
+  socket.on("disconnect", data => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Disconnect");
+  });
 
-    socket.on('send msg', (data) => {
-        io.sockets.emit('add msg', { msg: data });
-    });
+  socket.on("send msg", data => {
+    io.sockets.emit("add msg", { name: data.name, msg: data.msg });
+  });
 });
